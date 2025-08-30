2dff8f1501912610eb57e4a250211ca6279917c0
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-08-31 00:18:56 +0700

    update

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -232,6 +232,31 @@ export default function Ranking() {
             </div>
           </div>
         )}
+        {/* Tombol Hitung */}
+        {alternatives.length > 1 && (
+          <div className="text-center mt-6">
+            <button
+              onClick={calculate}
+              className="bg-[#446BA6] hover:bg-[#36588A] w-full flex justify-center items-center gap-2 text-white px-6 py-3 rounded-xl shadow-md transition"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z"
+                />
+              </svg>
+              Hitung Ranking
+            </button>
+          </div>
+        )}
 
         {/* Input Form */}
         <div className=" mx-auto bg-white shadow-lg text-[#446BA6] rounded-2xl py-8 px-14 border border-[#446BA6]">
@@ -305,32 +330,6 @@ export default function Ranking() {
             </button>
           </div>
         </div>
-
-        {/* Tombol Hitung */}
-        {alternatives.length > 1 && (
-          <div className="text-center mt-6">
-            <button
-              onClick={calculate}
-              className="bg-[#446BA6] hover:bg-[#36588A] w-full flex justify-center items-center gap-2 text-white px-6 py-3 rounded-xl shadow-md transition"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z"
-                />
-              </svg>
-              Hitung Ranking
-            </button>
-          </div>
-        )}
       </section>
 
       {/* Modal Hasil Ranking */}
